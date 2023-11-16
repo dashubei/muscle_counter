fa5502b6d81cc81f98d5df858094d3fcc6275d3f
dashubei <dashubei6@gmail.com>
2023-11-16 23:44:06 +0900

    okay

--- main.js
+++ main.js
@@ -1,42 +1,66 @@
 'use strict';
 
 {
-    //メニューと目標回数で追加するとlistでhtmlを生成する
-        //メニューと回数を取得して、その値を用いて、listを生成する
-        //その際に、id属性を付与した状態で連番にしてそれをinputの属性に入れて追加していく
-        const menu = document.getElementById('menu');
-        const targetNum = document.getElementById('targetNum');
-        const mainAdd = document.getElementById('mainAdd');
-        const ul = document.getElementById('ul');
-        
+    const menu = document.getElementById('menu');
+    const targetNum = document.getElementById('targetNum');
+    const mainAdd = document.getElementById('mainAdd');
+    const ul = document.getElementById('ul');
 
+    let buttonCounter = 0; // ボタンの連番を管理する変数
 
-        mainAdd.addEventListener('click', () => {
-            if (!(menu.value === "") && !(targetNum.value === "")) {
-                const newList = document.createElement('li');
-                const newSpan = document.createElement('span');
-                const newButton = document.createElement('button');
-                const newBr = document.createElement('br');
-                const newP = document.createTextNode(`残り:${targetNum.value}回`);
-                const newMenuText = document.createTextNode(menu.value);
-                const newTargetNumText = document.createTextNode(`目標回数：${targetNum.value}回`);
-                const newInput = document.createElement('input');
-        
-                newSpan.appendChild(newMenuText);
-                newButton.textContent = "追加";
-                newList.appendChild(newSpan);
-                newList.appendChild(newTargetNumText);
-                newList.appendChild(newInput);
-                newList.appendChild(newButton);
-                newList.appendChild(newBr);
-                newList.appendChild(newP);
-                ul.appendChild(newList);
-        
-                menu.value = "";
-                targetNum.value = "";
-            } else {
-                console.log("二つとも入力してね");
-            }
-        });
-        
+    mainAdd.addEventListener('click', () => {
+        if (!(menu.value === "") && !(targetNum.value === "")) {
+            const newList = document.createElement('li');
+            const newSpan = document.createElement('span');
+            const newButton = document.createElement('button');
+            const newBr = document.createElement('br');
+            const newP = document.createElement('p');
+            const newH2 = document.createElement('h2');
+            const P = document.createElement('p');
+            const newMenuText = document.createTextNode(menu.value);
+            const newTargetNumText = document.createTextNode(`目標回数：${targetNum.value}回`);
+            const newInput = document.createElement('input');
+
+            let remainingCount = parseInt(targetNum.value); // 残り回数を初期化
+
+            newSpan.appendChild(newMenuText);
+            P.appendChild(newTargetNumText);
+            newButton.textContent = "完了";
+            newList.appendChild(newSpan);
+            newList.appendChild(P);
+            newList.appendChild(newInput);
+            newList.appendChild(newButton);
+            newList.appendChild(newBr);
+            newList.appendChild(newP);
+            ul.appendChild(newList);
+
+            // ボタンのIDに連番を付与
+            const buttonId = `newMenu-${buttonCounter}`;
+            newButton.id = buttonId;
+            buttonCounter++;
+
+            newButton.addEventListener('click', () => {
+                if ((remainingCount - newInput.value) >= 0) {
+                    remainingCount -= newInput.value;
+                    newInput.value = '';
+                    newP.textContent = `残り:${remainingCount}回`;
+
+                    // 残り回数が0になった場合、処理を中止
+                    if (remainingCount === 0) {
+                        newP.textContent = 'お疲れ様!!!!';
+                        newButton.disabled = true; // ボタンを無効化
+                        newButton.classList.add('done');
+                    }
+                }
+            });
+
+            // 初期の残り回数を表示
+            newP.textContent = `残り:${remainingCount}回`;
+
+            menu.value = "";
+            targetNum.value = "";
+        } else {
+            console.log("二つとも入力してね");
+        }
+    });
 }
