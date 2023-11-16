5a832772edc96be3cb2300612fa45e1ea75cba2d
dashubei <dashubei6@gmail.com>
2023-11-16 18:24:29 +0900

    何となく途中です

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
 'use strict';
 
 {
-    const pushUp = document.getElementById('pushUp');
-    const curl = document.getElementById('curl');
-    const press = document.getElementById('press');
-    let chestCount = 100;
-    let armCount = 100;
-    let sholderCount = 100;
-    
-    pushUp.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const Num = chestCount - (document.getElementById('a').value);
-        document.getElementById('chest').textContent = Num;
-        chestCount = chestCount- (document.getElementById('a').value);
-    });
-    curl.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const Num = armCount - (document.getElementById('b').value);
-        document.getElementById('arm').textContent = Num;
-        armCount = armCount- (document.getElementById('b').value);
-    });
-    press.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const Num = sholderCount - (document.getElementById('c').value);
-        document.getElementById('sholder').textContent = Num;
-        sholderCount = sholderCount- (document.getElementById('c').value);
-    });
+    //メニューと目標回数で追加するとlistでhtmlを生成する
+        //メニューと回数を取得して、その値を用いて、listを生成する
+        //その際に、id属性を付与した状態で連番にしてそれをinputの属性に入れて追加していく
+        const menu = document.getElementById('menu');
+        const targetNum = document.getElementById('targetNum');
+        const mainAdd = document.getElementById('mainAdd');
+        const ul = document.getElementById('ul');
+        
+
+
+        mainAdd.addEventListener('click', () => {
+            if (!(menu.value === "") && !(targetNum.value === "")) {
+                const newList = document.createElement('li');
+                const newSpan = document.createElement('span');
+                const newMenu = document.createTextNode(menu.value);
+                const newTargetNum = document.createTextNode(targetNum.value);
+                const newInput = document.createElement('input');
+                newList.appendChild(newSpan);
+                newList.appendChild(newMenu);
+                newList.appendChild(newTargetNum);
+                newList.appendChild(newInput);
+                console.log(newList);
+                ul.appendChild(newList);
+                menu.value = "";
+                targetNum.value = "";
+            } else {
+                console.log("二つとも入力してね");
+            }
+
+        });
 }
